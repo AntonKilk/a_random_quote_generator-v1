9521f3ba7e3901728286bbf991bf491fa22d6728
AntonKilk <ant.kilk@gmail.com>
2019-06-20 01:24:49 +0300

    Script. Changed

--- js/script.js
+++ js/script.js
@@ -6,23 +6,22 @@ project 1 - A Random Quote Generator
 // Function "printQuote" takes a quote from function "getRandomQuote" and formats it for printing
 function printQuote (){
   var printQuote = getRandomQuote();
-    var HTML = '<div id="quote-box"> <p class="quote">Quote: ' + printQuote.quote + '</p>';
-  HTML += '<p class="source">Source: ' + printQuote.source + '</p>';
+    var HTML = '<p class="quote">' + printQuote.quote + '</p>';
+  HTML += '<p class="source">' + printQuote.source + '</p>';
   
   //checks if property 'year' and "citation" is in the 'quotes' array
   for (var prop in printQuote){
   if (prop === "year"){
-    HTML += '<span class="year">Year: ' + printQuote.year + '</span>';
+    HTML += '<span class="year">' + printQuote.year + '</span>';
   }
   else if (prop === "citation"){
-    HTML += '<span class="citation">Citation: ' + printQuote.citation + '</span>';
+    HTML += '<span class="citation">' + printQuote.citation + '</span>';
   }
   }
-  HTML +='</div>';
+document.getElementById('quote-box').innerHTML = HTML;
   return HTML;
   }
   
-  console.log(printQuote());
   
   // Function "getRandomQuote" takes a random quote from array "quotes" and returns it.
   function getRandomQuote( ) {
